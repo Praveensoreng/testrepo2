4707ea3360734e33a98bb9cb35148b46d5420f05
Praveen Soreng <praveensoreng9@gmail.com>
2022-04-11 22:32:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+console.log("working")
+
+//in do while statement the body is executed first and then the conditon is checked;
+//in do while loop it has to execute the statement for atleast once.
+
+// let i = 0;
+
+// do{
+//     console.log(i);
+//     i++;
+
+// }
+// while(i < 10);
+
+
+// do{
+//     marks = marks + 10;
+// }
+// while(marks > 40);
+
+
+let i = 1;
+while( i <= 10){
+    console.log(i * 2);
+    i++;
+}
+
+let a = 1;
+do{
+    console.log(a * 2);
+    a++;
+}
+while(a <= 10);
+
+
+for(let c = 1; c <= 10; c++ ){
+    console.log(c * 2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
